f03220924d52344e746592a2fecca7bda9c73aba
Edi <82562025+edisaso@users.noreply.github.com>
2022-11-28 14:25:00 +0200

    Add files via upload

---
+++ Health.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 100;
+        public int playerHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = maxHealth;
+
+    }
+    public void PLayerTakeDamage(int damage)
+    {
+        playerHealth = playerHealth - damage;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
